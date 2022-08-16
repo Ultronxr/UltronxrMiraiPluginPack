41a532ce9bab76b165c1b76c3fc83bda609dc816
ultronxr <Ultronxr@users.noreply.github.com>
2022-08-16 13:17:12 +0800

    build.gradle.kts内容修改及依赖更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
-java{
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 plugins {
-    val kotlinVersion = "1.5.10"
+    val kotlinVersion = "1.6.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.11.1"
+    id("net.mamoe.mirai-console") version "2.12.1"
+    //id("net.mamoe.mirai-core") version "2.12.1"
 }
 
 group = "cn.ultronxr"
@@ -26,26 +22,38 @@ dependencies {
     //implementation("org.springframework.boot:spring-boot-starter-aop:2.5.5")
 
     // 日志
-    implementation("org.slf4j:slf4j-log4j12:1.7.35")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+    implementation("org.slf4j:slf4j-log4j12:1.7.36")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
 
     // 数据序列化
-    implementation("com.fasterxml.jackson.core:jackson-core:2.11.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.3")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.3")
-    implementation("org.apache.commons:commons-lang3:3.9")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
 
     // 开发工具/插件
-    implementation("org.projectlombok:lombok:1.18.24")
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
-    testCompileOnly("org.projectlombok:lombok:1.18.24")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
-    implementation("cn.hutool:hutool-all:5.5.8")
+    implementation("cn.hutool:hutool-all:5.8.3")
 
     // 二维码
-    implementation("com.google.zxing:core:3.4.1")
-    implementation("com.google.zxing:javase:3.4.1")
+    implementation("com.google.zxing:core:3.5.0")
+    implementation("com.google.zxing:javase:3.5.0")
 
+
+
+    // 测试环境依赖
+    testCompileOnly("org.projectlombok:lombok:1.18.24")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+
+}
+
+java{
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+mirai {
+    jvmTarget = JavaVersion.VERSION_11
 }
